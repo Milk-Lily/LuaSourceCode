9e804fcfb62411117d5d7e3544a49b81d78a5407
DESKTOP-JMRD4VS\MilkLily <386992702@qq.com>
2025-03-02 15:46:53 +0800

    init

---
+++ TestLua.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//int main() {
+//	lua_State* L = luaL_newstate();
+//	luaL_openlibs(L);
+//
+//	luaL_dofile(L, "testlua.lua");
+//
+//	lua_close(L);
+//
+//	return 0;
+//}
---
+++ testlua.lua
@@ -0,0 +1,6 @@
+print("on do file testlua.lua")
+
+local a = 10
+local b = a
+b = 11
+print("a = " .. a)
