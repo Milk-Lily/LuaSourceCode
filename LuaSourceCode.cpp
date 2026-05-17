6d5ab158193df7872974ccac0e113a1b89fb4add
MilkLily <386992702@qq.com>
2026-05-17 15:57:47 +0800

    接受kill信号量

--- LuaSourceCode.cpp
+++ LuaSourceCode.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <csignal>
+#include <sys/select.h>
 
 extern "C" {
 #include "lua.h"
@@ -10,10 +12,41 @@ extern "C" {
 #include "lauxlib.h"
 }
 
+static lua_State* gL = nullptr;
+
+// Ctrl+C 时通过 lua_sethook 在下一条 Lua 指令处注入错误，干净退出
+static void sigint_handler(int)
+{
+    if (gL) {
+        lua_sethook(gL, [](lua_State* L, lua_Debug*) {
+            lua_sethook(L, nullptr, 0, 0);
+            luaL_error(L, "interrupted");
+        }, LUA_MASKCALL | LUA_MASKRET | LUA_MASKCOUNT, 1);
+    }
+}
+
+// 注册到 Lua 的 sleep(seconds) 函数
+// 使用 select 实现：Linux 信号会中断 select，使 Ctrl+C 立即响应
+static int lua_sleep(lua_State* L)
+{
+    double seconds = luaL_checknumber(L, 1);
+    struct timeval tv;
+    tv.tv_sec  = (long)seconds;
+    tv.tv_usec = (long)((seconds - tv.tv_sec) * 1e6);
+    select(0, nullptr, nullptr, nullptr, &tv);
+    return 0;
+}
+
 int main()
 {
     lua_State* L = luaL_newstate();
+    gL = L;
+    signal(SIGINT, sigint_handler);
     luaL_openlibs(L);
+
+    // 将 sleep 函数注册为全局函数供 Lua 调用
+    lua_pushcfunction(L, lua_sleep);
+    lua_setglobal(L, "sleep");
 
     std::cout << "Starting Lua: lua/Entry.lua\n";
 
--- lua/test.lua
+++ lua/test.lua
@@ -9,8 +9,5 @@ local tick = 0
 while true do
     tick = tick + 1
     print(string.format("[tick %d] os.time = %d", tick, os.time()))
-    -- os.execute("sleep 1") 依赖 shell，改用纯 Lua 忙等确保跨平台
-    -- 但 Lua 标准库没有 sleep；使用 os.clock 做忙等（CPU 占用高）
-    -- 推荐方式：os.execute 在 Linux 上可用
-    os.execute("sleep 1")
+    sleep(1)   -- C++ 注册的 sleep，SIGINT 可立即中断
 end
